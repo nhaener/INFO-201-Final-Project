07b228975411d8dabc2f0470e1204f8806f48761
Nick Haener <nhaener@uw.edu>
2018-05-30 19:11:48 -0700

    created my data analysis file

---
+++ scripts/analysisNick.R
@@ -0,0 +1,19 @@
+#####################################
+## Nick Haener
+## INFO 201 - Final Project
+## Spring 2018
+##
+## Nick's analysis file for data processing, 
+## testing and grapgical display testing
+#####################################
+
+################# SETUP ##############
+# set working directory
+# setwd("C:/Users/MyPC/Desktop/INFO201/InfoFinalProject/INFO-201-Final-Project")
+
+# Load needed packages
+library(plotly)
+library(dplyr)
+
+
+#read in data sets
