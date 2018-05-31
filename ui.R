6edcbd8007a2dd85ccd04271945a27a9e3198c67
jamestlee513 <jamestlee513@gmail.com>
2018-05-30 20:58:02 -0700

    Completed documentation in ui.R

--- ui.R
+++ ui.R
@@ -136,20 +136,37 @@ shinyUI(fluidPage(theme = "bootstrap.css", #sets theme for web app
                                       mainPanel( 
                                         #HTML Formatted Text
                                         h1("Project Documentation"),
-                                        h3("Created by: Nick Haener, and team"),
+                                        h3("Created by: Nick Haener, James Lee, Douglas Pham and Ryan Siu"),
                                         br(), 
-                                        h2("Project Description"), 
+                                        h2("Project Description"),
+                                        p("This project uses data about NCAA School's atheltic funding and NCAA School's student attendance.
+                                          After some data manipulation and analysis we were able analyze and display the relationship of these,
+                                          and visualize the data so that we are able to draw conclusions and answers about the schools in that
+                                          scope."),
                                         h3("What is the data?"),
-                                        p(" The data provided shows.... "), 
-                                        p("For a quick link to the site", 
-                                          tags$a(href = "Insert link to data here!", "Click Here!")),
-                                        p("This data set is based on information from..... "),
+                                        p("The data views the major NCAA Schools' revenue and expenditure trends in correlation to those school's 
+                                          attendance from the years 2006-2015, with data collected on a yearly basis."), 
+                                        p("For a quick link to NCAA's School's athletic funding", 
+                                          tags$a(href = "http://spendingdatabase.knightcommission.org/reports/63a3c708", 
+                                                 "Click Here!")),
+                                        p("This data set is based on information released spending logs from each major NCAA school included in
+                                          the data."),
+                                        p("For a quick link to post-secondary education data", 
+                                          tags$a(href = "https://catalog.data.gov/dataset/201112-integrated-postsecondary-education-data-system", 
+                                                 "Click Here!")),
+                                        p("This data set is based on information released data from all post-secondary education (any education
+                                          past high school. i.e. undergraduate or postgraduate educations) institutions."),
                                         p("The data sets we used are:"), 
                                         tags$ul(
                                           tags$li("data 1"), 
                                           tags$li("data 2")),
                                         h3("Why do we care?"),
-                                        p(" "),
+                                        p("The one of the main concepts we aimed to address was, how the ranking of college sports teams matched up to the
+                                          ranking of the college. Perhaps potential student athletes are searching for the best colleges to attend
+                                          that suit their goals. Another was to observe and show how colleges spend their money. Tuition payers and donors would
+                                          probably like to know how their money is being spent! And the last main idea was if certain conference/region have
+                                          better funding, and if that correlates at all to more admitted students to those colleges. It could possibly indicate
+                                          a better school if there's more private funding which could be information students and parents would like to know!"),
                                         br(),
                                         h2("Technical Description"),
                                         h3("What libraries were used?"),
@@ -164,7 +181,10 @@ shinyUI(fluidPage(theme = "bootstrap.css", #sets theme for web app
                                           tags$a(href = "https://cran.rstudio.com/web/packages/dplyr/vignettes/introduction.html", "dplyr"), "."
                                         ),
                                         h3("What aspects of the data sets were used?"),
-                                        p("we focused mainly on ...", 
+                                        p("We focused mainly on using the attendance and total athletic spending for the overview,
+                                          athletic spending per athlete, total athletic spending, number of football players, and the
+                                          NCAA subdivision for the athletics tab, and academic spending, people attending and division
+                                          for the academics tab.", 
                                           tags$ul(
                                             tags$li(""),
                                             tags$li(" ")
